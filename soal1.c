20d85cf2bad4ff6d5e1046da1e18db12bf972141
khalil-97 <khalilalfaiz79@gmail.com>
2026-05-06 10:26:38 +0700

    Refactor main function to handle input and output

--- soal1.c
+++ soal1.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
 
-int main(void) {
+int main() {
+    int N;
+    
+    if (scanf("%d", &N) != 1) {
+        return 1; 
+    }
+    
+    char id[N][20];
+    int durasi[N];
+   
+    for (int i = 0; i < N; i++) {
+        scanf("%s %d", id[i], &durasi[i]);
+    }
+    
+   
+    printf("ORDER");
+    for (int i = 0; i < N; i++) {
+        printf(" %s", id[i]);
+    }
+  
+    int sekarang = 0; 
+    for (int i = 0; i < N; i++) {
+       
+
+        sekarang += durasi[i];
+    }
+    
+    printf("WAIT %d\n", sekarang);
+    
     return 0;
 }
